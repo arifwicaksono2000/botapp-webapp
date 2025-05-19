8a8cea6b2f8862f88bc7a934d57336f0ffb875fe
arifwicaksono2000 <arifwsno@gmail.com>
2025-05-20 00:04:34 +0700

    commit

--- djbotapp/botcore/consumers.py
+++ djbotapp/botcore/consumers.py
@@ -9,7 +9,10 @@ class BotStatusConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("bot_status", self.channel_name)
 
-    # this name matches type="send_status" in broadcast_trades()
     async def send_status(self, event):
         await self.send(text_data=json.dumps(event["data"]))
+
+    async def send_position_update(self, event):
+        await self.send(text_data=json.dumps(event["data"]))
+
 
